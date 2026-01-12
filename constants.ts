37717e9dc4a07d10ddbc08f998b00f17223f3950
kuchmanalexander <kuchmanalexander7@gmail.com>
2026-01-12 23:55:43 +0300

    feat: добавление системных констант и параметров симуляции API

---
+++ constants.ts
@@ -0,0 +1,13 @@
+
+export const SESSION_COOKIE_NAME = 'session_token';
+export const API_SIMULATION_LATENCY = 500;
+
+// Permission strings based on the task description
+export const PERMISSIONS = {
+  USER_LIST_READ: 'user:list:read',
+  USER_DATA_READ: 'user:data:read',
+  COURSE_INFO_WRITE: 'course:info:write',
+  COURSE_TEST_WRITE: 'course:test:write',
+  QUEST_CREATE: 'quest:create',
+  TEST_QUEST_UPDATE: 'test:quest:update',
+};
